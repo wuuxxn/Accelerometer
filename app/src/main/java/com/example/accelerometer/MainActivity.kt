dae1d8c19381e8d8ac6c71d81b5215bd39c8dbed
wuuxxn <owen20050723@gmail.com>
2024-12-16 15:52:44 +0800

    1

--- app/src/main/java/com/example/accelerometer/MainActivity.kt
+++ app/src/main/java/com/example/accelerometer/MainActivity.kt
@@ -10,6 +10,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -18,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.platform.LocalContext
+import com.example.accelerometer.ui.theme.AccelerometerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
